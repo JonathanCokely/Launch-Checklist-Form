111430791bf2a131e68369c32bc9821f15ddd72c
Cokely <JC032926@cerner.net>
2020-09-10 19:20:24 -0500

    Project update 9/10/2020 - 7:19 pm

--- script.js
+++ script.js
@@ -58,6 +58,25 @@ window.addEventListener("load", function(){
          faultyItems.style.visibility = "hidden";
          launchStatus.innerHTML = `Shuttle is ready for launch`;
          launchStatus.style.color = "green";
+
+               //Fetch planetary JSON data
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            response.json().then(function(json){
+               const missionTarget = document.getElementById("missionTarget");
+               missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[0].name}</li>
+                  <li>Diameter: ${json[0].diameter}</li>
+                  <li>Star: ${json[0].star}</li>
+                  <li>Distance from Earth: ${json[0].distance}</li>
+                  <li>Number of Moons: ${json[0].moons}</li>
+               </ol>
+               <img src="${json[0].image}"></img>`
+            })
+         })
+
+         event.preventDefault();
       }
 
       //Makes Faulty Items Visible && h2 "launchStatus" changes to "Shuttle not ready for launch" and color = red when fuelLevel<10000
